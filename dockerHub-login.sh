6b924ff69549dfdcd402be9cb5e9cfbee145af27
Aritra Biswas <biswas.aritra09@gmail.com>
2022-04-14 22:33:29 +0530

    Update dockerHub-login.sh

--- dockerHub-login.sh
+++ dockerHub-login.sh
@@ -1,13 +1,13 @@
 #!bin/bash
-# "current users" should be a art of docker group else use sudo before run docker command
 # -----------------------------------------
-#DOCKER_PASS='A3.WnNf.NpRb4X-'
-#DOCKER_USER='tcghub'
-#echo $DOCKER_PASS | docker login -u$DOCKER_USER --password-stdin $DOCKER_HOST
+## README ##
+# "current users" should be a part of docker group else - use sudo before run docker command
+# COMMAND SYNTAX =  dockerHub-login.sh <DOCKER_HUB_USER_ID> <DOCKER_HUB_PASS>
 # -----------------------------------------
 docker logout
-DOCKER_USER=$1
-DOCKER_PASS=$2
 #DOCKER_PASS='A3.WnNf.NpRb4X-'
 #DOCKER_USER='tcghub'
+DOCKER_USER=$1
+DOCKER_PASS=$2
 echo $DOCKER_PASS | docker login -u$DOCKER_USER --password-stdin
+#echo $DOCKER_PASS | docker login -u$DOCKER_USER --password-stdin $DOCKER_HOST
