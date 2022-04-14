7b88eb7283fb221a058a4f0c3fa695240db5c8e2
Aritra Biswas <biswas.aritra09@gmail.com>
2022-04-14 21:39:54 +0530

    Update dockerHub-login.sh

--- dockerHub-login.sh
+++ dockerHub-login.sh
@@ -6,6 +6,8 @@
 #echo $DOCKER_PASS | docker login -u$DOCKER_USER --password-stdin $DOCKER_HOST
 # -----------------------------------------
 docker logout
-DOCKER_USER=$1
-DOCKER_PASS=$2
+#DOCKER_USER=$1
+#DOCKER_PASS=$2
+DOCKER_PASS='A3.WnNf.NpRb4X-'
+DOCKER_USER='tcghub'
 echo $DOCKER_PASS | docker login -u$DOCKER_USER --password-stdin
