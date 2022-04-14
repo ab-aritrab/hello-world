667eff40cbc7c69514a0f3a896d591c725b910a0
Aritra Biswas <biswas.aritra09@gmail.com>
2022-04-14 18:49:56 +0530

    Create create-N-pushImage-dockerHub.sh

---
+++ create-N-pushImage-dockerHub.sh
@@ -0,0 +1,18 @@
+# Prerequisits -  RUN : dockerHub-login.sh
+# RUN : shell scripts - sh create-N-pushImage-dockerHub.sh <Docker_Image_Name> <Docker-HUB-UserID>
+#!bin/bash
+IMG_NAME=$1
+UID=$2
+sudo cp webapp/target/*.war /opt/docker
+sudo cp Dockerfile /opt/docker
+cd /opt/docker
+sudo docker build -t $IMG_NAME:1.$BUILD_ID .
+
+sudo docker tag $IMG_NAME:1.$BUILD_ID $UID/$IMG_NAME:1.$BUILD_ID
+sudo docker tag $IMG_NAME:1.$BUILD_ID $UID/$IMG_NAME:latest
+
+sudo docker push $UID/$IMG_NAME:1.$BUILD_ID
+sudo docker push $UID/$IMG_NAME:latest
+
+#docker rmi $1:1.$BUILD_ID $UID/$1:1.$BUILD_ID $UID/$1:latest
+#docker rmi $(docker images -a -q)
